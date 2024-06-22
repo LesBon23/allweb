9c9dd8a9d849ba245e571993a26c1b48e55004a8
LesBon23 <162440125+LesBon23@users.noreply.github.com>
2024-06-21 19:15:21 -0700

    Update Main.js

--- websitefiles/Main.js
+++ websitefiles/Main.js
@@ -1,4 +1,4 @@
 function searchData() {
             const query = document.getElementById('searchInput').value;
-            window.location.href = `file:///F:/Improtent/ImportantOW/New/B.html?search=${encodeURIComponent(query)}`;
+            window.location.href = `https://lesbon23.github.io/allweb/websitefiles/B.html?search=${encodeURIComponent(query)}`;
         }
